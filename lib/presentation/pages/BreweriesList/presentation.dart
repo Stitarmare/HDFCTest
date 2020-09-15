405cd09e188f62ac47d56022d93b51bacb3ccbfc
saurabh titarmare <saurabh.titarmare@neosofttech.com>
2020-09-15 23:16:13 +0530

    COMMIT:change in presentation

--- lib/presentation/pages/BreweriesList/presentation.dart
+++ lib/presentation/pages/BreweriesList/presentation.dart
@@ -1,33 +1,22 @@
 import 'dart:convert';
 
-import 'package:hdfcTestApp/data/localdatasource/local_web_datasource.dart';
 import 'package:hdfcTestApp/data/model/list_brewerie_model.dart';
 import 'package:hdfcTestApp/domain/entity/list_reweries_entity.dart';
 import 'dart:js' as js;
 
 class BreweriesListPresentation {
-  LocalWebDatasource localWebDatasource = LocalWebDatasource();
-
   getData(Function(List<ListBreweriesModel>) list) async {
-    final value = localWebDatasource.getCookie("breweries") as String;
-    if (value != null && value != "") {
-      final jsonValue = json.decode(value);
-      final listBrew = List<ListBreweriesModel>.from(
-          jsonValue.map((x) => ListBreweriesModel.fromMap(x)));
-      list(listBrew);
-    } else {
-      js.context.callMethod('checkBroweserComapatiblity', [
-        (data) {
-          if (data != null) {
-            final jsonValue = json.decode(data);
-            final listBrew = List<ListBreweriesModel>.from(
-                jsonValue.map((x) => ListBreweriesModel.fromMap(x)));
-            list(listBrew);
-          } else {
-            list(null);
-          }
+    js.context.callMethod('checkBroweserComapatiblity', [
+      (data) {
+        if (data != null) {
+          final jsonValue = json.decode(data);
+          final listBrew = List<ListBreweriesModel>.from(
+              jsonValue.map((x) => ListBreweriesModel.fromMap(x)));
+          list(listBrew);
+        } else {
+          list(null);
         }
-      ]);
-    }
+      }
+    ]);
   }
 }
